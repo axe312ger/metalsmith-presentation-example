32b930d09f23a39bedb059a7678efba63736d2f9
Benedikt Rötsch <benedikt@contentful.com>
2018-01-19 13:17:31 +0100

    convert markdown into html

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Metalsmith = require('metalsmith')
+const markdown = require('metalsmith-markdown')
 
 Metalsmith(__dirname)
   .source('content')
   .destination('dist')
+  .use(markdown())
   .build(function(err) {
     if (err) throw err;
   });
