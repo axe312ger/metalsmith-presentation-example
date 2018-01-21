62b198c02a54e556ed196ea5997af9fd138fb5dd
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-01-21 14:27:05 +0100

    remove default values from layouts config

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ Metalsmith(__dirname)
     pageTitle: 'My Awesome Blog'
   })
   .use(layouts({
-    default: 'default.hbs',  // required
-    pattern: '**/*.html', // multimatch
-    directory: 'layouts', // default
-    engineOptions: {} // pass option to transformer
+    default: 'default.hbs'
   }))
   .build(function(err) {
     if (err) throw err;
