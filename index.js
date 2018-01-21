91de8229d31022a8d19be18f29a85ff466e66b34
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-01-21 14:50:50 +0100

    add live reload for content editing

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const Metalsmith = require('metalsmith')
 const debug = require('metalsmith-debug')
 const layouts = require('metalsmith-layouts')
 const markdown = require('metalsmith-markdown')
+const browserSync = require('metalsmith-browser-sync')
+
+const __DEV__ = process.env.NODE_ENV === 'development'
 
 Metalsmith(__dirname)
   .source('content')
   .destination('dist')
+  .use(__DEV__ && browserSync({
+    open: false,
+    server: {
+      baseDir: 'dist',
+      directory: true
+    },
+    files  : [
+      'content/**/*.md',
+      'layouts/**/*'
+    ]
+  }))
   .use(debug())
   .use(markdown())
   .metadata({
