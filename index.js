1f521eb35a670f78f8ce24cea7443a7ead19129d
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-01-21 14:26:07 +0100

    minimal layout implementation

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const Metalsmith = require('metalsmith')
-const markdown = require('metalsmith-markdown')
 const debug = require('metalsmith-debug')
+const layouts = require('metalsmith-layouts')
+const markdown = require('metalsmith-markdown')
 
 Metalsmith(__dirname)
   .source('content')
   .destination('dist')
   .use(debug())
   .use(markdown())
+  .metadata({
+    pageTitle: 'My Awesome Blog'
+  })
+  .use(layouts({
+    default: 'default.hbs',  // required
+    pattern: '**/*.html', // multimatch
+    directory: 'layouts', // default
+    engineOptions: {} // pass option to transformer
+  }))
   .build(function(err) {
     if (err) throw err;
   });
