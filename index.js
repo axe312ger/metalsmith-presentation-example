46243892851995d62963675e5753c5c5cf9129b9
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-01-21 13:25:02 +0100

    add debug output

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Metalsmith = require('metalsmith')
 const markdown = require('metalsmith-markdown')
+const debug = require('metalsmith-debug')
 
 Metalsmith(__dirname)
   .source('content')
   .destination('dist')
+  .use(debug())
   .use(markdown())
   .build(function(err) {
     if (err) throw err;
